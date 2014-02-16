6a9f9ce286b6c47397292d7f846e20e614f9c8ba
dario <dario.coco@gmail.com>
2014-02-16 06:10:53 +0100

    Debug mode set to false

--- addressbook/__init__.py
+++ addressbook/__init__.py
@@ -16,7 +16,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DEBUG=True,
+    DEBUG=False,
     SECRET_KEY='development key',
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 ))
